c25d2f339cdb8c87aa330baa4fd3ff4f1c5da704
thiagoe99 <thiagoe99@hotmail.com>
2021-06-04 15:15:14 -0300

    Montada estrutura jogo

--- estrutura_jogo.py
+++ estrutura_jogo.py
@@ -14,35 +14,57 @@ def initTabuleiro():
     return tabuleiro
 
 #Retorna X ou O
-def jogador(tabuleiro):
+def jogador(jogador):
+    if(jogador == 0):
+        return "x"
+    else:
+        return "o"
+    
+def imprimirTabuleiro(tabuleiro):
     for i in range(3):
-        for j in range(3):
-            if(tabuleiro[i][j] == 1):
-                return "X"
-            else:
-                return "O"
+        print("|".join(tabuleiro[i]))
+        if(i<2):
+            print("______")
 
-#Retorna todas as jogadas disponíveis
-def acoes(tabuleiro):
+#Verifica se posição indicada esta dentro do tabuleiro
+def verificaJogadaValida(indice):
+    if(indice < 0 or indice >2):
+        print("Indice invalido! Informe um indice entre 0 e 2.")
+        return False
+    return True
+
+#Verifica se posição do tabuleiro esta ocupada
+def verificaPosicaoVazia(tabuleiro, i, j):
+    if(tabuleiro[i][j] != vazia):
+        print("Indice invalido! Posicao ja ocupada.")
+        return False
+    return True
+
+#Retorna o tabuleiro que resulta ao fazer a jogada i,j
+def resultado(tabuleiro, i, j, njogador):
+    tabuleiro[i][j] = jogador(njogador)
+    imprimirTabuleiro(tabuleiro)
+
+def aindaPossuiPosicaoVazia(tabuleiro):
+    qtdPosicoesOcupadas: 0
     for i in range(3):
         for j in range(3):
             if(tabuleiro[i][j] == vazia):
-                print("Posicao: ".join(tabuleiro[i][j].join(" disponível!")))
+                qtdPosicoesOcupadas = qtdPosicoesOcupadas + 1
+    return qtdPosicoesOcupadas
 
-#Retorna o tabuleiro que resulta ao fazer a jogada i,j
-def resultado(tabuleiro, acao):
+def quemGanhou(tabuleiro):
+    if(aindaPossuiPosicaoVazia(tabuleiro) != 9):
+        return vazia
+    if(tabuleiro[0][0] != vazia and tabuleiro[0][0] == tabuleiro[1][1] and tabuleiro[1][1] == tabuleiro[2][2]):
+        return tabuleiro[0][0]
+    if(tabuleiro[0][2] != vazia and tabuleiro[0][2] == tabuleiro[1][1] and tabuleiro[1][1] == tabuleiro[2][0]):
+        return tabuleiro[0][2]
+    for i in range(3):
+        if(tabuleiro[i][0] != vazia and tabuleiro[i][0] == tabuleiro[i][1] and tabuleiro[i][1] == tabuleiro[i][2]):
+            return tabuleiro[i][0]
+    for i in range(3):
+        if(tabuleiro[0][i] != vazia and tabuleiro[0][i] == tabuleiro[1][i] and tabuleiro[1][i] == tabuleiro[2][i]):
+            return tabuleiro[0][i]
+    return "EMPATE"
 
-#Retorna o ganhador, se houver
-def ganhador(tabuleiro):
-
-#Retorna Verdadeiro se o jogo acabou, Falso caso contrário
-def final(tabuleiro):
-
-#Retorna 1 se X ganhou, -1 se 0 ganhou, 0 caso contrário.
-def custo(tabuleiro):
-
-#Retorna a jogada ótima para o jogador atual
-def minimax(tabuleiro):
-
-def maxValor(tabuleiro):
-def minValor(tabuleiro):
