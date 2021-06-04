ccfa5af0d88102d7beb9ea63b2c58f0d20c8c6ce
thiagoe99 <thiagoe99@hotmail.com>
2021-06-04 16:42:03 -0300

    funções prontas jogo da velha

--- busca_competitiva.py
+++ busca_competitiva.py
@@ -1,4 +1,47 @@
 #  Exercício 2: Busca Competitiva  (Jogo da Velha) 
 # Aluno: Thiago Emanuel
+from estrutura_jogo import final,vazia,imprimirTabuleiro, initTabuleiro, leEntrada, quemGanhou, resultado, verificaPosicaoVazia
 
-#
+jogador = 0
+tabuleiro = initTabuleiro()
+jganhador = quemGanhou(tabuleiro)
+
+#funcao iniciar jogo
+def iniciaJogo(jogador,tabuleiro):
+    imprimirTabuleiro(tabuleiro)
+    print("----==----")
+    while(final(tabuleiro)):
+        if(jogador == 0):
+            i = leEntrada("Informe o valor da linha: ")
+            j = leEntrada("Informe o valor da coluna: ")
+        else:
+            i = leEntrada("Informe o valor da linha: ")
+            j = leEntrada("Informe o valor da coluna: ")
+        if(verificaPosicaoVazia(tabuleiro,i,j)):
+            resultado(tabuleiro,i,j,jogador)
+            print("----==----")
+            jogador = (jogador+1)%2
+
+    return quemGanhou(tabuleiro)
+
+resultadofinal = iniciaJogo(jogador,tabuleiro)
+
+#Retorna 1 se X ganhou, -1 se 0 ganhou, 0 caso contrário.
+def custo(resultadofinal):
+    if(resultadofinal == "x"):
+        return 1
+    elif(resultadofinal == "o"):
+        return -1
+    return 0
+
+#Retorna o ganhador, se houver
+def ganhador(numerojogador):
+    print("xxxxxxxxxxxxxxxxxx")
+    imprimirTabuleiro(tabuleiro)
+    if(numerojogador == 1):
+        print("O ganhador é o jogador: 1")
+    else:
+        print("O ganhador é o jogador: 2")
+    print("xxxxxxxxxxxxxxxxxx")
+
+ganhador(custo(resultadofinal))
--- estrutura_jogo.py
+++ estrutura_jogo.py
@@ -14,12 +14,12 @@ def initTabuleiro():
     return tabuleiro
 
 #Retorna X ou O
-def jogador(jogador):
+def labelJogador(jogador):
     if(jogador == 0):
         return "x"
     else:
         return "o"
-    
+#imprime tabuleiro
 def imprimirTabuleiro(tabuleiro):
     for i in range(3):
         print("|".join(tabuleiro[i]))
@@ -33,29 +33,53 @@ def verificaJogadaValida(indice):
         return False
     return True
 
+#le entrada do jogador
+def leEntrada(entrada):
+    try:
+        indice = int(input(entrada))
+        if(verificaJogadaValida(indice)):
+            return indice
+        leEntrada(entrada)
+    except:
+        return leEntrada(entrada)
+            
+#Retorna todas as jogadas disponíveis
+def acoes(tabuleiro):
+    for i in range(3):
+        for j in range(3):
+            if(tabuleiro[i][j] == vazia):
+                print("Posicao: ".join(tabuleiro[i][j].join(" disponível!")))
+
 #Verifica se posição do tabuleiro esta ocupada
 def verificaPosicaoVazia(tabuleiro, i, j):
     if(tabuleiro[i][j] != vazia):
-        print("Indice invalido! Posicao ja ocupada.")
+        print("Indice invalido! Posição ja ocupada.")
+        acoes(tabuleiro)
         return False
     return True
 
 #Retorna o tabuleiro que resulta ao fazer a jogada i,j
 def resultado(tabuleiro, i, j, njogador):
-    tabuleiro[i][j] = jogador(njogador)
+    tabuleiro[i][j] = labelJogador(njogador)
     imprimirTabuleiro(tabuleiro)
 
+#verifica se ainda possui posicao vazia no tabuleiro
 def aindaPossuiPosicaoVazia(tabuleiro):
     qtdPosicoesOcupadas: 0
     for i in range(3):
         for j in range(3):
             if(tabuleiro[i][j] == vazia):
-                qtdPosicoesOcupadas = qtdPosicoesOcupadas + 1
-    return qtdPosicoesOcupadas
+                qtdPosicoesOcupadas += 1
+    if(qtdPosicoesOcupadas != 9):
+        return True
+    else:
+        print("xxxxxxxxxxxxxxxxxx")
+        print("O jogo empatou!")
+        print("xxxxxxxxxxxxxxxxxx")
+        return False
 
+#verifica quem ganhou, se deu empate ou o ainda possui posicão vazia
 def quemGanhou(tabuleiro):
-    if(aindaPossuiPosicaoVazia(tabuleiro) != 9):
-        return vazia
     if(tabuleiro[0][0] != vazia and tabuleiro[0][0] == tabuleiro[1][1] and tabuleiro[1][1] == tabuleiro[2][2]):
         return tabuleiro[0][0]
     if(tabuleiro[0][2] != vazia and tabuleiro[0][2] == tabuleiro[1][1] and tabuleiro[1][1] == tabuleiro[2][0]):
@@ -66,5 +90,10 @@ def quemGanhou(tabuleiro):
     for i in range(3):
         if(tabuleiro[0][i] != vazia and tabuleiro[0][i] == tabuleiro[1][i] and tabuleiro[1][i] == tabuleiro[2][i]):
             return tabuleiro[0][i]
-    return "EMPATE"
+
+#Retorna Verdadeiro se o jogo acabou, Falso caso contrário
+def final(tabuleiro):
+    if(aindaPossuiPosicaoVazia(tabuleiro) and (quemGanhou(tabuleiro) != "x" or quemGanhou(tabuleiro) != "o")):
+        return False
+    return True
 
