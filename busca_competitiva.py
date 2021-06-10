b3fedef85a3719f54c183cc1d6dc91ba208e5683
thiagoe99 <thiagoe99@hotmail.com>
2021-06-09 22:21:31 -0300

    Corrigido erro funcao inicia jogo

--- busca_competitiva.py
+++ busca_competitiva.py
@@ -1,6 +1,7 @@
 #  Exercício 2: Busca Competitiva  (Jogo da Velha) 
 # Aluno: Thiago Emanuel
-from estrutura_jogo import final,vazia,imprimirTabuleiro, initTabuleiro, leEntrada, quemGanhou, resultado, verificaPosicaoVazia
+from minimax import movimentoIA, custo
+from estrutura_jogo import final,imprimirTabuleiro, initTabuleiro, leEntrada, quemGanhou, resultado, verificaPosicaoVazia
 
 jogador = 0
 tabuleiro = initTabuleiro()
@@ -10,37 +11,30 @@ jganhador = quemGanhou(tabuleiro)
 def iniciaJogo(jogador,tabuleiro):
     imprimirTabuleiro(tabuleiro)
     print("----==----")
-    while(final(tabuleiro)):
-        if(jogador == 0):
-            i = leEntrada("Informe o valor da linha: ")
-            j = leEntrada("Informe o valor da coluna: ")
+    while final(tabuleiro):
+        if jogador == 0:
+            i,j = movimentoIA(tabuleiro, jogador)
+            #i = leEntrada("Informe o valor da linha: ")
+            #j = leEntrada("Informe o valor da coluna: ")
         else:
             i = leEntrada("Informe o valor da linha: ")
             j = leEntrada("Informe o valor da coluna: ")
-        if(verificaPosicaoVazia(tabuleiro,i,j)):
+        if verificaPosicaoVazia(tabuleiro,i,j):
             resultado(tabuleiro,i,j,jogador)
             print("----==----")
             jogador = (jogador+1)%2
 
     return quemGanhou(tabuleiro)
 
-#Retorna 1 se X ganhou, -1 se 0 ganhou, 0 caso contrário.
-def custo(resultadofinal):
-    if(resultadofinal == "x"):
-        return 1
-    elif(resultadofinal == "o"):
-        return -1
-    return 0
-
 #Retorna o ganhador, se houver
 def ganhador(numerojogador):
-    print("xxxxxxxxxxxxxxxxxx")
-    if(numerojogador == 1):
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    if numerojogador == 1:
         print("O ganhador é o jogador: 1")
-    elif(numerojogador == -1):
+    elif numerojogador == -1:
         print("O ganhador é o jogador: 2")
     else:
         print("O jogo empatou!")
-    print("xxxxxxxxxxxxxxxxxx")
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
 ganhador(custo(iniciaJogo(jogador,tabuleiro)))
